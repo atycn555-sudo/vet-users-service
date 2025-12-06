56ea19f83093e37c3008837ab566e6e996247ea8
atycn555-sudo <atycn555@gmail.com>
2025-12-05 20:01:59 -0600

    Update ModelMapperConfig.java

--- src/main/java/com/example/vet/config/ModelMapperConfig.java
+++ src/main/java/com/example/vet/config/ModelMapperConfig.java
@@ -9,8 +9,6 @@ public class ModelMapperConfig {
 
     @Bean
     public ModelMapper modelMapper() {
-        // Al tener los campos en el DTO con nombres exactos (veterinarianFirstName),
-        // ModelMapper hace la conversión automáticamente sin necesidad de configuración extra.
         return new ModelMapper(); 
     }
 }
